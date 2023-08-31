d475c13d4528a86c706b2c73d42ddee2d96f84f6
awabaqibb <94635789+awabaqibb@users.noreply.github.com>
2023-08-31 17:57:54 +0500

    ok1

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,17 +5,15 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const FormControl = styled.div`
-    .form-control {
-      margin: 0.5rem 0;
-    }
+    margin: 0.5rem 0;
 
-    &.label {
+    & label {
       font-weight: bold;
       display: block;
       margin-bottom: 0.5rem;
     }
 
-    &.input {
+    & input {
       display: block;
       width: 100%;
       border: 1px solid #ccc;
@@ -24,7 +22,7 @@ const CourseInput = (props) => {
       padding: 0 0.25rem;
     }
 
-    &.input:focus {
+    & input:focus {
       outline: none;
       background: #fad0ec;
       border-color: #8b005d;
@@ -47,4 +45,32 @@ const CourseInput = (props) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
-    setEnteredValue(event.target.
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
+      </FormControl>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
